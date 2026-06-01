c1fe600f2f80e10fae23784fc20b44d905d4c62c
sshlio <43551169+sshlio@users.noreply.github.com>
2026-06-02 01:06:51 +0200

    Index the one

--- nvim/lua/config/jumps.lua
+++ nvim/lua/config/jumps.lua
@@ -1,4 +1,6 @@
 local group = vim.api.nvim_create_augroup('_billy_jumps', { clear = true })
+local timer = vim.uv.new_timer()
+local DELAY = 100  -- ms after cursor stops
 
 local lines_by_buf = {}
 
@@ -16,6 +18,46 @@ end
 
 my_move = false
 
+local moved = function()
+  local state = lines_for(vim.api.nvim_get_current_buf())
+  local line = vim.api.nvim_win_get_cursor(0)[1]
+  local diff = math.abs(state.prev_line - line)
+
+  if state.index >= 0 then
+    state.lines = vim.list_slice(state.lines, 0, #state.lines - state.index)
+
+    state.index = -1
+  end
+
+  local lines = state.lines
+
+  if diff < 2 then
+
+    local last = state.lines[#lines]
+
+    local view = {
+       line = line,
+       -- view = vim.fn.winsaveview()
+    }
+
+    if not last then
+      table.insert(lines, view)
+      return
+    end
+
+
+    if math.abs(last.line - line) > 2 then
+      -- branch off
+      table.insert(lines, view)
+    else
+      last.line = view.line
+      last.view = view.view
+    end
+  end
+
+  state.prev_line = line
+end
+
 vim.api.nvim_create_autocmd({ 'CursorMoved'}, {
   group = group,
   callback = function(ev)
@@ -23,46 +65,10 @@ vim.api.nvim_create_autocmd({ 'CursorMoved'}, {
       my_move = false
       return
     end
-
-    vim.schedule(function()
-      local state = lines_for(ev.buf)
-      local line = vim.api.nvim_win_get_cursor(0)[1]
-      local diff = math.abs(state.prev_line - line)
-
-      if state.index >= 0 then
-        state.lines = vim.list_slice(state.lines, 0, #state.lines - state.index)
-
-        state.index = -1
-      end
-
-      local lines = state.lines
-
-      if diff < 2 then
-
-        local last = state.lines[#lines]
-
-        local view = {
-           line = line,
-           -- view = vim.fn.winsaveview()
-        }
-
-        if not last then
-          table.insert(lines, view)
-          return
-        end
-
-
-        if math.abs(last.line - line) > 2 then
-          -- branch off
-          table.insert(lines, view)
-        else
-          last.line = view.line
-          last.view = view.view
-        end
-      end
-
-      state.prev_line = line
-    end)
+    timer:stop()
+    timer:start(DELAY, 0, vim.schedule_wrap(function()
+      moved()
+    end))
   end,
 })
 
@@ -71,6 +77,9 @@ local seek = function(diff)
 
   local lines = state.lines
   local line = vim.api.nvim_win_get_cursor(0)[1]
+  if state.index == -1 then
+    moved()
+  end
 
   state.index = state.index + diff
 
